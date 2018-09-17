5ac71b787e1a4c13b4e1947fe09b73ca765972f5
shamsuddha <shamsuddha@gmail.com>
2018-09-17 11:33:50 +0600

    dashboard design and travel event page design updated, option menu added in every activity

--- app/src/main/java/com/example/shamsuddha/tourmate/ProfileActivity.java
+++ app/src/main/java/com/example/shamsuddha/tourmate/ProfileActivity.java
@@ -1,7 +1,11 @@
 package com.example.shamsuddha.tourmate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +13,54 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //------------------ Menu Section ------------------------------------
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.action_home:
+                Intent home = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(home);
+                break;
+            default:
+                Toast.makeText(this, "Something Went ", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //------------------ Menu Section End --------------------------------
+
+
+
+
 }
