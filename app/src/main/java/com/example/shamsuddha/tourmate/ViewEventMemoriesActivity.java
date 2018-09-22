3d6143851086b3c4665d6aba4f3bd2eaf6935eba
shamsuddha <shamsuddha@gmail.com>
2018-09-22 23:54:57 +0600

    home page change

--- app/src/main/java/com/example/shamsuddha/tourmate/ViewEventMemoriesActivity.java
+++ app/src/main/java/com/example/shamsuddha/tourmate/ViewEventMemoriesActivity.java
@@ -1,13 +1,72 @@
 package com.example.shamsuddha.tourmate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ViewEventMemoriesActivity extends AppCompatActivity {
 
+
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_event_memories);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //------------------ Menu Section ------------------------------------
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_after_login,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.action_home:
+                Intent home = new Intent(ViewEventMemoriesActivity.this, MainActivity.class);
+                startActivity(home);
+                break;
+
+            case R.id.action_logout:
+                firebaseAuth.signOut();
+                finish();
+                Intent signOut = new Intent(ViewEventMemoriesActivity.this, MainActivity.class);
+                startActivity(signOut);
+                break;
+
+
+
+            default:
+                Toast.makeText(this, "Something Went ", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //------------------ Menu Section End --------------------------------
+
+
 }
